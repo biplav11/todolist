4725fdd769cc65c8702c478574d43ad2c256e0e8
Biplav Sharma <nifty@Biplavs-MacBook-Pro.local>
2019-07-14 20:08:37 +0545

    Add put method implementation to project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+Axios.defaults.baseURL = 'https://5b8971636b7dcb0014d5f3f1.mockapi.io'
+
+export default class App extends Component {
+    state = {
+        todos: []
+    }
+
+    apiCall = () => {
+        Axios.get('/todos')
+        .then(result =>{
+            console.log(result)
+            this.setState({
+                todos: result.data
+            })
+
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+    }
+
+    componentDidMount(){
+        this.apiCall()
+    }
+
+    handleStatus = (id, status) => {
+        console.log(id)
+        Axios.put('/todos/'+id, {
+            completed : !status
+        })
+        .then(data => {
+            console.log(data);
+            this.apiCall();
+        })
+
+    }
+
+    render() {
+        let {todos} = this.state
+        return (
+            <>
+                <header>
+                    <h1>Todo List</h1>
+                </header> 
+                <main>
+                    <ul>
+                        {
+                            todos.map(todo => {
+                                let {id, title, completed} = todo
+                                let className = completed ? "completed" : ''
+                                return (
+                                    <li onClick={() => this.handleStatus(id,completed)} className={className} key={id}>{title}</li>
+                                )
+                            })
+                        }
+                    </ul>
+                </main>
+            </>
+        )
+    }
 }
-
-export default App;
